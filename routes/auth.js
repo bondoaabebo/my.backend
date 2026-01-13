5e15c916c3484a50b538ed03e65ada7ca9179a71
bondoaabebo <galal.galgola@gmail.com>
2026-01-13 16:54:03 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import License from '../models/license.js';
+import License from '../models/License.js';
 import Device from '../models/device.js';
 
 const router = express.Router();
